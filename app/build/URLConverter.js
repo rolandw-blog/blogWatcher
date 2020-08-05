e9e23d34f96f62745ac0ba0d139c2cf37585eebd
RolandWarburton <warburtonroland@gmail.com>
2020-08-05 23:36:34 +1000

    :sparkles: Build out the URL converter for GH link

--- app/build/URLConverter.js
+++ app/build/URLConverter.js
@@ -1,14 +1,28 @@
 // Different functions to extract useful file names from urls
 const debug = require("debug")("blogWatcher:URLConverter");
+const path = require("path");
 
 /**
  *
  * @param {String} url - Github URL to convert to page name
  * @example RawgithubUserContentToPageName("https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Writing/bookmarks.md")
+ * @returns ["writing", "bookmarks"]
  */
 const githubUserContentToPageName = (url) => {
+	// Turn it into a URL
 	const urlObject = new URL(url);
-	// debug(urlObject);
+
+	// Get the pathname (/RolandWarburton/knowledge/master/Writing/bookmarks.md)
+	const pathArray = urlObject.pathname.split("/");
+
+	// Remove the junk elements at the start
+	pathArray.splice(0, 4);
+
+	// Strip the extension from the last page name
+	const pageName = pathArray[pathArray.length - 1];
+	pathArray[pathArray.length - 1] = path.parse(pageName).name;
+
+	debug(pathArray);
 };
 
-module.exports = githubUserContentToPageName;
+module.exports = { githubUserContentToPageName };
