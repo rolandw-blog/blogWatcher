30ad28bb4a69c6ec463b173474aef12d01fae538
RolandWarburton <warburtonroland@gmail.com>
2020-08-05 17:33:34 +1000

    :recycle: Cleaned the trainwreck that was prevSite Advice and help from nickmcm92 on GH

--- app/build/previewSiteLayout.js
+++ app/build/previewSiteLayout.js
@@ -1,34 +1,73 @@
 // Returns JSON structure of what the website will look like based on the database of sources
 const debug = require("debug")("blogWatcher:previewSiteLayout");
 const getAllPages = require("../queries/getAllPages");
-const { json } = require("express");
+const merge = require("lodash.merge");
 
+/**
+ * Converting an absolute path into a json object [] => {}
+ *
+ *  THANKS TO nickmcm92 for helping me with this function‼
+ * @param {String} websitePath - A (preferebly) absolute path to a page on the website
+ * @example urlToJson(/sample/too/page)
+ */
+// ! THANKS TO nickmcm92 for helping me with this function‼
+// ! https://github.com/nickmcm92
+const urlToJson = (websitePath) => {
+	const pathArray = websitePath.split("/");
+
+	// Remove the first element as a websitePath should start with "/" which splits into ['', 'path']
+	if (pathArray[0] === "") pathArray.splice(0, 1);
+
+	// store the new json path as a string which can be parsed to JSON later
+	var jsonstr = "";
+
+	// construct a string of json values
+	for (var i = pathArray.length - 1; i >= 0; i--) {
+		jsonstr = `"${pathArray[i]}": {${jsonstr}}`;
+	}
+	// wrap the string of json values in a json object
+	jsonstr = `{${jsonstr}}`;
+
+	// convert it into JSON
+	return JSON.parse(jsonstr);
+};
+
+/**
+ * Returns a JSON object that represents the full websites path structure
+ */
 const previewSiteLayout = async () => {
 	debug("running previewSiteLayout");
+
+	// get every page from the database
 	const pages = await getAllPages();
-	const currentJson = {};
+
+	// this is where each JSON path is stored as an individual object
+	const allJson = [];
+	// let prevItems = "";
 	for (page of pages) {
-		const pathArray = page.websitePath.split("/");
+		const newJson = urlToJson(page.websitePath);
+		allJson.push(newJson);
 
-		// remove the first element as a websitePath should start with "/" which splits into ['', 'path']
-		if (pathArray[0] === "") pathArray.splice(0, 1);
-
-		let prevItems = "";
-
-		// ! oh man this is so bad
-		// if you are reading this please dont judge me too hard
-		// - Its bad practice
-		// - Its a terrible code smell
-		// - Its vulnerable to malicious attacks / code injection
-		for (let i in pathArray) {
-			prevItems += `[pathArray[${i}]]`;
-			if (!eval(`currentJson${prevItems}`)) {
-				eval(`currentJson${prevItems} = {}`);
-			}
-		}
+		debug(`parsed ${page.websitePath} to a json path`);
 	}
-	debug(currentJson);
-	return currentJson;
+
+	debug("Finished formatting the path map:");
+	debug(JSON.stringify(merge(...allJson), null, 2));
+
+	return merge(...allJson);
 };
 
 module.exports = previewSiteLayout;
+
+// An example of exported data from previewSiteLayout()
+//
+// {
+// 	sample: {
+// 		too: {
+// 			page: {},
+// 		},
+// 		three: {
+// 			page: {},
+// 		},
+// 	},
+// };
