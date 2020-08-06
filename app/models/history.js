2993f484f57ea056691458cd01ec85a36d20fe47
RolandWarburton <warburtonroland@gmail.com>
2020-08-06 22:45:23 +1000

    :construction: ADD mongoose  history schema

---
+++ app/models/history.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const schema = {
+	commit: {
+		// a JSON string is submitted
+		type: String,
+		require: true,
+	},
+};
+
+const historySchema = mongoose.Schema(schema, {
+	collection: "pages",
+	autoCreate: true,
+});
+
+const History = mongoose.model("Page", historySchema);
+
+module.exports = { History, schema };
