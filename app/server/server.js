aa66f0ab1d4b2c46d975e09606aa12dae3581bf9
RolandWarburton <warburtonroland@gmail.com>
2020-08-06 21:39:31 +1000

    :fire: Remove temp hook test from server.js added it as a route under /hooks

--- app/server/server.js
+++ app/server/server.js
@@ -2,53 +2,28 @@ const { connectToDB } = require("../database/database");
 const debug = require("debug")("blogWatcher:server");
 const path = require("path");
 const bodyParser = require("body-parser");
+const express = require("express");
 const cors = require("cors");
-const urlConverter = require("../build/URLConverter");
 const pageRoutes = require("../routes/pageRoutes.js");
 const webhooks = require("../routes/webhooks");
-const crypto = require("crypto");
-const buildPages = require("../build/buildPages");
-const previewSiteLayout = require("../build/previewSiteLayout");
 require("dotenv").config();
 
-const express = require("express");
-
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// stuff for testing
+const buildPages = require("../build/buildPages");
+const previewSiteLayout = require("../build/previewSiteLayout");
+const urlConverter = require("../build/URLConverter");
 
 const app = express();
-// app.options("*", cors());
 const corsOptions = {
 	origin: "*",
 };
 app.use(cors(corsOptions));
 
 app.use("/", pageRoutes);
-// app.use("/", webhooks);
-app.use(bodyParser.json());
+app.use("/hooks", webhooks);
 
 // quick and dirty upload form
 app.use("/upload", express.static(path.resolve(process.env.ROOT, "public")));
-
-function verifyGithubPayload(req, res, next) {
-	debug("running GH payload verify middleware");
-
-	const payload = JSON.stringify(req.body);
-	let sig =
-		"sha1=" +
-		crypto
-			.createHmac("sha1", process.env.GITHUB_SECRET)
-			.update(payload.toString())
-			.digest("hex");
-
-	debug(sig);
-	debug(req.headers["x-hub-signature"]);
-	if (req.headers["x-hub-signature"] == sig) {
-		debug("all good");
-	} else {
-		debug("all bad");
-	}
-	return next();
-}
 
 const server = async () => {
 	await connectToDB(
@@ -70,12 +45,6 @@ const server = async () => {
 		});
 	});
 
-	app.post("/", [urlencodedParser, verifyGithubPayload], (req, res) => {
-		res.status(200).json({
-			success: true,
-		});
-	});
-
 	// ! testing stuff
 
 	// previewSiteLayout();
@@ -83,9 +52,9 @@ const server = async () => {
 	// pull down all the markdown
 	// buildPages();
 
-	urlConverter.githubUserContentToPageName(
-		"https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Writing/bookmarks.md"
-	);
+	// urlConverter.githubUserContentToPageName(
+	// 	"https://raw.githubusercontent.com/RolandWarburton/knowledge/master/Writing/bookmarks.md"
+	// );
 
 	// urlConverter.gistContentToPageName(
 	// 	"https://gist.githubusercontent.com/RolandWarburton/0f113f46fd780b24973830ab1794dee3/raw/723f35a59ba7a62e258b17d5a55eb9d2d5534964/test.md"
