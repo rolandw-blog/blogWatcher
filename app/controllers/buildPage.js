261b5c72561748f832345161a191c1db59d056ea
RolandWarburton <warburtonroland@gmail.com>
2020-08-22 01:14:45 +1000

    :fire: Clean up buildPage

--- app/controllers/buildPage.js
+++ app/controllers/buildPage.js
@@ -7,7 +7,6 @@ const { v4 } = require("uuid");
 
 const buildPage = async (req, res) => {
 	debug("=======================");
-	// debug(` ${req.params.id}`);
 	// TODO make an event emitter to .on("progress") and send data back to the client
 	//
 	// example of sending data back
@@ -22,24 +21,8 @@ const buildPage = async (req, res) => {
 	//   }, 1000);
 
 	const page = await buildPageFunction(req.params.id);
-	// const body = { id: req.params.id, entropy: v4() };
-	// const params = new URLSearchParams(body);
-
-	// // sign the body
-	// const sig = signPayload(body);
-
-	// // create a header object
-	// debug("setting headers");
-	// const headers = {
-	// 	"x-payload-signature": sig,
-	// };
-
-	// tell the website to build this page
-	// await fetch(`http://192.168.0.100:2020/build/${req.params.id}`, {
-	// 	method: "POST",
-	// 	body: params,
-	// 	headers: headers,
-	// }).then((res) => res.json());
+	// debug(req.params);
+	// debug(page);
 
 	return res.status(200).json({
 		success: true,
