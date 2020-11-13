122349f91fd4d2a50bb1e12660b4398e9cff0909
RolandWarburton <warburtonroland@gmail.com>
2020-11-13 03:26:48 +0000

    :wrench: Increase per_page query to 999

--- app/queries/getAllPages.js
+++ app/queries/getAllPages.js
@@ -32,16 +32,16 @@ const getSkipCount = (page, per_page) => {
 };
 
 /** Sanitizes the per_page query to return the number of items available per page,
- * or the maximum number of items which can be returned on a page (Capped at 20)
+ * or the maximum number of items which can be returned on a page (Capped at 999)
  * @param {number} per_page - The number of items available per page
  * @returns The number of items available per page
  */
 const getPerPage = (per_page) => {
 	per_page = Number(per_page);
-	if (per_page <= 20) {
+	if (per_page <= 999) {
 		return per_page;
 	} else {
-		return 20;
+		return 999;
 	}
 };
 
