cac7c83c50b151d6b4e414c81101aa52819bd8ca
RolandWarburton <warburtonroland@gmail.com>
2020-12-02 05:12:14 +0000

    :fire: Remove old code from getPagesFilter

--- app/controllers/getPagesFilter.js
+++ app/controllers/getPagesFilter.js
@@ -36,21 +36,6 @@ const getPagesFilter = async (req, res) => {
 			.status(400)
 			.json({ success: false, error: "its dead jim! 🔨" });
 	}
-
-	// // check that BS isnt being passed in
-	// if (req.query.websitePath) filters.websitePath = req.query.websitePath;
-	// if (req.query.regex)
-	// 	filters.websitePath = new RegExp(req.query.websitePath);
-	// // ? add more stuff...
-
-	// debug(`getting pages using the filter: "${JSON.stringify(filters)}"`);
-	// const pages = await getAllPages(filters, queries);
-	// const count = await countPages();
-	// if (pages) return res.status(200).json({ count: count, data: pages });
-	// else
-	// 	return res
-	// 		.status(400)
-	// 		.json({ success: false, error: "its dead jim! 🔨" });
 };
 
 module.exports = getPagesFilter;
