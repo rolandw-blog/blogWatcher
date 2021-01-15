226e3653b69d3040ea64e7d5d6058423561223ff
RolandWarburton <warburtonroland@gmail.com>
2021-01-15 20:47:42 +1100

    :recycle: Rewrite updatePageData

--- app/queries/updatePageData.js
+++ app/queries/updatePageData.js
@@ -1,5 +1,6 @@
 const { Page } = require("../models/page");
 const debug = require("debug")("blogWatcher:query");
+const util = require("util");
 require("dotenv").config();
 
 const updatePageDate = async (filter, update) => {
@@ -8,17 +9,13 @@ const updatePageDate = async (filter, update) => {
 	debug({ filter, update });
 
 	// find the page by its filter {_id: aaabbbccc} and then update {somefiled: newvalue}
-	return await Page.findOneAndUpdate(filter, update, (err, result) => {
-		if (err) {
-			debug("an error happened :O");
-			debug(err);
-			return undefined;
-		} else {
-			debug(`saved document`);
-			debug(result);
-			return result;
-		}
-	});
+	try {
+		const result = await Page.findOneAndUpdate(filter, update).exec();
+		debug(`new Object: ${JSON.stringify(result)}`);
+		return result;
+	} catch (err) {
+		return new Error("Failed to update page somehow");
+	}
 };
 
 module.exports = updatePageDate;
