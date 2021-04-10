53ff70e4c7a7e5179f6f31f92e12498ee857cca6
RolandWarburton <warburtonroland@gmail.com>
2021-04-10 16:03:52 +1000

    :wrench: Add .vscode launch.json for in-container debugging with VSC node attach-to-process

--- app/controllers/UpdateRoutes/common/updatePage.js
+++ app/controllers/UpdateRoutes/common/updatePage.js
@@ -10,7 +10,7 @@ const updatePageController = async (req, res) => {
 
 	// update the page
 	const document = await updatePage(req.body.filter, req.body.update);
-	if (!document) return res.status(400).json({message: "document was not found"});
+	if (!document) return res.status(400).json({ message: "document was not found" });
 	else res.status(200).json(document);
 };
 
